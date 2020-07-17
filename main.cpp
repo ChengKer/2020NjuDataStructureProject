d0fa5904595559b13b4728bae6817f3c6adf0024
ChengKer <564157441@qq.com>
2020-07-17 08:32:25 +0800

    add SelectInfo TestFunc

---
+++ main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+#include<string>
+#include"Graph/graph.h"
+
+void select_info_test(Graph &g) {
+	cout << "开始测试！" << endl;
+	while (true) {
+		string temp;
+		cin >> temp;
+		cout << g.select_info(temp) << endl;
+	}
+}
+
+int main()
+{
+	string province_addr = "D:/大三下/数据结构/期末项目/INFO.txt";
+	string distance_addr = "D:/大三下/数据结构/期末项目/省距离.txt";
+	Graph g(province_addr, distance_addr);
+	select_info_test(g);
+}
